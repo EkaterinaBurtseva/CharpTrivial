e5e01a39cec864f0a2e897d5d2ed65d161503c94
Kate Burtseva <kateryna.burtseva@itera.no>
2018-03-08 14:07:54 +0200

    initial commit

---
+++ Lesson1/if_statement/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace If_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 24;
+            int number2 = 20;
+            if (number1 > number2)
+            {
+                Console.WriteLine("number1 is greater than number2");
+            }
+
+           
+        }
+    }
+}
