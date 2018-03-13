16ab357c2c2348fff05de9fd08e5bfcb270f38ca
Kate Burtseva <kateryna.burtseva@itera.no>
2018-03-13 08:51:25 +0200

    task1

---
+++ Lesson1/Ifelse_Statement/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ifelse_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestIfElese(5);
+        }
+        public static void TestIfElese(int n)
+        {
+            if (n < 10)
+            {
+                Console.WriteLine("n isless than 10");
+            }
+            else if (n < 20)
+            {
+                Console.WriteLine("n is less than 20");
+            }
+            else if (n < 30)
+            {
+                Console.WriteLine("n is less than 30");
+            }
+            else
+            {
+                Console.WriteLine("n is greater than or equal to 30");
+            }
+        }
+    }
+}
---
+++ Lesson1/SwitchStatement/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SwitchStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestSwitch(2, 3, '*');
+        }
+        public static void TestSwitch(int op1, int op2, char opr) {
+            int result;
+            switch (opr)
+            {
+                case '+':
+                    result = op1 + op2;
+                    break;
+                case '-':
+                    result = op1 - op2;
+                    break;
+                case '*':
+                    result = op1 * op2;
+                    break;
+                case '/':
+                    result = op1 / op2;
+                    break;
+                default:
+                    Console.WriteLine("Unknown Operator");
+                    return;
+            }
+            Console.WriteLine("Result:{0}", result);
+            return;
+            }
+        }
+    }
+
