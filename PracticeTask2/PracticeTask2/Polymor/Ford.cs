90ab76deef1ed2379b2db228c9066bc3ffa1c181
Kate Burtseva <kateryna.burtseva@itera.no>
2018-04-15 22:59:04 +0300

    s

--- PracticeTask2/PracticeTask2/Polymor/Ford.cs
+++ PracticeTask2/PracticeTask2/Polymor/Ford.cs
@@ -1,28 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PracticeTask2
 {
-    class Ford : Car
+    class Car2 : Car
     {
         string color2 = "blue";
 
-        public Ford(int type):base (type){
-        }
+        public Car2(int type):base (type){
+}
 
-        public Ford(int type, int speed)
-        {
-            this.type = type;
-            this.speed = speed;
-        }
         public override void PrintColor()
         {
             Console.WriteLine(color2);
 
+        }
+
+        public override void Drive()
+        {
+            speed = 100;
         }
      
         
