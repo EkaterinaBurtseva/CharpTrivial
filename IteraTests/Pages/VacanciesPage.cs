96f33f87b6a7385c1d9d5371f0ea76cbcd5489ab
Kate Burtseva <kateryna.burtseva@itera.no>
2018-05-03 23:10:01 +0300

    1

--- IteraTests/Pages/VacanciesPage.cs
+++ IteraTests/Pages/VacanciesPage.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework.Api;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.PageObjects;
 
@@ -14,9 +15,26 @@ namespace Pages
         [FindsBy(How = How.CssSelector, Using = "section.margin--header")]
         private IWebElement VacanciesHeader;
 
+        [FindsBy(How = How.ClassName, Using = "row bg--white")]
+        private IWebElement VacanciesBlock;
+
+        [FindsBy(How = How.ClassName, Using = "block__text__bottom")]
+        private IWebElement VacanciesItems;
+
+        [FindsBy(How = How.ClassName, Using = "block__text__bottom")]
+        private List<IWebElement> VacanciesTitles;
+
         public bool IsVacanciesPageDisplayed()
         {
             return VacanciesHeader.Displayed;
+        }
+
+        public void NumberOfNetVacancies()
+        {
+            var count = 0;
+            if(VacanciesTitles.Contains(""))
+            return count;
+
         }
     }
 }
--- IteraTests/YesTest/VacanciesTest.cs
+++ IteraTests/YesTest/VacanciesTest.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using NUnit.Framework.Internal;
 using Pages;
 
 namespace YesTest
 {
+    [TestFixture]
     class VacanciesTest
     {
+
+        private string title = ".NET";
         [Test]
         public void VacanciesStartTest()
         {
