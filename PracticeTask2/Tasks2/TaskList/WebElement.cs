8972dabb4bb53dfc5a9b28ffb779901c41e0ad33
Kate Burtseva <kateryna.burtseva@itera.no>
2018-06-13 09:02:26 +0300

    reorganized folders, created class Column and WebElement

---
+++ PracticeTask2/Tasks2/TaskList/WebElement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks2.TaskList
+{
+    public class WebElement
+    {
+        string Xpath { get; set; }
+        string Text { get; set; }
+        string ClassName { get; set; }
+        string Name { get; set; }
+    }
+}
