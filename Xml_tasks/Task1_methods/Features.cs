1f7bc4e67b28ad230bd492c3329328f90e1018fb
Kate Burtseva <kateryna.burtseva@itera.no>
2018-07-07 22:15:26 +0300

    created methods for read and modify xml

---
+++ Xml_tasks/Task1_methods/Features.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Task1_methods
+{
+    public class Features
+    {
+        public void ReadNodeXpath()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("example.xml");
+            XmlNodeList nodeList = doc.SelectNodes("/bookstore/book[@publicationdate='1981']/title");            
+            foreach (XmlNode node in nodeList)
+            {
+                Console.WriteLine(node.InnerText);
+               // Console.WriteLine("First-name: " + node["first-name"].InnerText + " Last Name: " + node["last-name"].InnerText);
+            }
+                      
+
+        }
+           public  void ReadValueByXpath()
+            {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("example.xml");
+            string myVal = ((XmlElement)doc.DocumentElement.SelectSingleNode("descendant::bookstore/book/author/xpath")).Value;
+            Console.WriteLine(myVal);
+
+        }
+
+        public void AddNodeByXpath()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("example.xml");
+            XmlNode node = doc.CreateNode(XmlNodeType.Element, "book", null);
+            XmlNode nodeTitle = doc.CreateElement("title");
+            nodeTitle.InnerText = "This title is created by code";
+            XmlNode nodeUrl = doc.CreateElement("author");
+            node.AppendChild(nodeTitle);
+            node.AppendChild(nodeUrl);
+            doc.DocumentElement.AppendChild(node);
+            doc.Save("example.xml");
+
+        }
+
+        public void ModifyValueByXpath()
+        {
+
+        }
+        }
+    }
+
---
+++ Xml_tasks/Task1_methods/Main1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Task1_methods
+{
+    public class Main1
+    {
+        static void Main(string[] args)
+        {
+            Features features = new Features();
+            features.ReadValueByXpath();
+            Console.ReadLine();
+        }
+
+    }
+}
+
