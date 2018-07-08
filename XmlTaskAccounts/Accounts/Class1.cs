e1824cf2598832b63fa3b2643f0c73df5a2ccd08
Kate Burtseva <kateryna.burtseva@itera.no>
2018-07-08 17:22:03 +0300

    added methods to add, edit nodes in xml

---
+++ XmlTaskAccounts/Accounts/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounts
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            Methods methods = new Methods();
+            methods.CreateAccountAttribute();           
+            methods.EditAndAddNewNode();
+            methods.EditAndUpdateNode();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ XmlTaskAccounts/Accounts/Methods.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Accounts
+{
+    public class Methods
+    {
+        private XmlDocument doc;
+
+        public Methods(/*path*/)
+        {
+            this.doc = new XmlDocument();
+            doc.Load("Account.xml");
+        }
+
+
+        public void CreateAccountAttribute()
+        {
+            XmlNode node = doc.CreateNode(XmlNodeType.Element, "Account", null);
+            XmlAttribute name = doc.CreateAttribute("name");
+            name.Value = "Test";
+            XmlNode nodeFirstName = doc.CreateElement("FistName");
+            nodeFirstName.InnerText = "Merry";
+            node.Attributes.Append(name);
+            node.AppendChild(nodeFirstName);
+            doc.DocumentElement.AppendChild(node);
+            doc.Save("Account.xml");
+            doc.Save(Console.Out);
+        }
+
+        public void EditAndAddNewNode()
+        {
+            XmlNode node = doc.SelectSingleNode("Accounts/Account[@name='Katerina']");
+            XmlNode job = doc.CreateNode(XmlNodeType.Element, "Job", null);
+            node.AppendChild(job);
+            doc.DocumentElement.AppendChild(node);
+            job.InnerText = "QA";
+            doc.Save("Account.xml");
+            doc.Save(Console.Out);
+        }
+
+        public void EditAndUpdateNode()
+        {
+            XmlNode node = doc.SelectSingleNode("Accounts/Account[@name='Katerina']/Job");
+            node.InnerText = "Software Developer";
+            doc.Save("Account.xml");
+            doc.Save(Console.Out);
+        }
+    }
+}
