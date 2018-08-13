c2db3aeee0a888240c59805234281e5e8da08e7e
Kate Burtseva <kateryna.burtseva@itera.no>
2018-08-13 23:19:34 +0300

    hidden and override examples

---
+++ HiddenOverride/HiddenOverride/ICar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiddenOverride
+{
+    interface ICar
+    {
+
+        int Speed { get; set; }
+        void Drive();
+        void OpenDoor();
+        string Name { get; set; }
+
+        int GetMaxSpeed();
+
+        void CarryBaggage();
+    }
+}
