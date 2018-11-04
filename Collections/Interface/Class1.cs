567d2bcab027456f9e5aa82a985435cc4e6d9cdf
Kate Burtseva <kateryna.burtseva@itera.no>
2018-11-04 22:18:01 +0200

    inheritence from two interfaces with the same named method

---
+++ Collections/Interface/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Class1:Interface1,Interface2
+    {
+        public void Foo()
+        {
+            Console.WriteLine("test");
+        }
+
+        public void Foo(string abc)
+        {
+            Console.WriteLine(abc);
+        }
+    }
+}
