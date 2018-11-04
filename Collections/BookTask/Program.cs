61ab5ab7ef79ea9fa5021f8a53a027a9ef24b1b8
Kate Burtseva <kateryna.burtseva@itera.no>
2018-11-04 23:46:06 +0200

    test

---
+++ Collections/BookTask/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> book = new Dictionary<string, string>()
+            {
+                { "test", "test2" } ,
+              
+            };
+            foreach (KeyValuePair<string, string> keyValue in book)
+            {
+               
+                string bookName = keyValue.Key;
+                string bookAuthor = keyValue.Value;
+
+                Book col = new Book() { BookName = bookName, BookAuthor = bookAuthor};
+                Console.WriteLine(col);
+            }
+            book.Add("testinh", "tsgjghj");
+            Console.ReadLine();
+        }
+    }
+}
