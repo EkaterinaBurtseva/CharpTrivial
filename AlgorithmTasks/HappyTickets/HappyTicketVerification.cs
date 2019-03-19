6c366e5fce68ff2f73bd1c9f8a98575106515a99
Kateryna Burtseva <kateryna.burtseva@itera.no>
2019-03-19 11:17:44 +0200

    implemented HappyTicket and made carcas for other  tasks

---
+++ AlgorithmTasks/HappyTickets/HappyTicketVerification.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyTickets
+{
+    public class HappyTicketVerification
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your ticket number");
+            var ticketNumber = Console.ReadLine();
+            string part1 = ticketNumber.Substring(0, 3);
+            string part2 = ticketNumber.Substring(3, 3);
+            int firstPart = Convert.ToInt32(part1);
+            int secondPart = Convert.ToInt32(part2);
+
+            int sumFirst = 0;
+            while (firstPart != 0)
+            {
+                sumFirst += firstPart % 10;
+                firstPart /= 10;
+            }
+
+            int sumSecond = 0;
+            while (secondPart != 0)
+            {
+                sumSecond += secondPart % 10;
+                secondPart /= 10;
+            }
+
+            if (sumFirst == sumSecond)
+            {
+                Console.WriteLine("Congrats!You have a happy ticket :)");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, ticket isn't happy.");
+            }
+
+
+        }
+    }
+}
---
+++ AlgorithmTasks/SumOf3And5/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOf3And5
+{
+    public class Class1
+    {
+    }
+}
